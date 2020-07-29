939f2d63cad085b5f7e295196043fc3b32001988
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-29 17:29:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+  #binding.pry
+  value.each do |color_key, name_array|
+#binding.pry 
+name_array.each do |name|
+  #binding.pry
+  if !final_array[name]
+    final_array[name]={}
+    #creates the hash for the name and checks to see if it is already there first
+  end
+  if !final_array[name][key]
+    final_array[name][key]=[]
+  end
+  final_array[name][key].push(color_key.to_s)
 end
+#!!binding.pry
+end
+end
+
+end
+
+
