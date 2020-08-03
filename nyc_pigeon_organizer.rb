550ea47762696e1883046764ee31bef7afafd3ca
Valerie Nuccio <val.nuccio@gmail.com>
2020-08-03 01:07:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,7 +27,7 @@ def nyc_pigeon_organizer(data)
   final_results=data.each_with_object ({}) do |(key,value), final_array|
     value.each do |inner_key, name_array|
       name_array.each do |name|
-        if !final_array[name] ("theo")
+        if !final_array[name]
           final_array[name] = {}
         end
         if !final_array[name][key]
