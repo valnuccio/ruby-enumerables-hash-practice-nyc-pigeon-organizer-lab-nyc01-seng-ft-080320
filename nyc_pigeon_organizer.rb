75493254932e685eab92c8532224d940796d1d1f
Valerie Nuccio <val.nuccio@gmail.com>
2020-08-03 01:09:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,7 +33,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           !final_array[name][key]=[]
         end
-        final_array.push [inner_key]
+        final_array[name][key].push [inner_key]
       end
   end
 end
